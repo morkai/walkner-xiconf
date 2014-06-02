e49473743b22921c825e149e96fc00b0d365f58e
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2014-06-02 12:35:14 +0200

    Fixed the installer not adding the OpenSSL `libeay32.dll` file near  the `node_modules/bin/ursaNative.node` file.

--- config/installer.js
+++ config/installer.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var fs = require('fs');
+var path = require('path');
+
 exports.autoIt3Wrapper = 'C:/Program Files (x86)/AutoIt3/SciTE/AutoIt3Wrapper/AutoIt3Wrapper.exe';
 exports.makensis = 'C:/Program Files (x86)/NSIS/makensis.exe';
 exports.googleChromePortable = 'C:/Programs/GoogleChromePortable';
@@ -7,6 +10,9 @@ exports.node = 'C:/Program Files (x86)/nodejs/node.exe';
 exports.nssm = 'C:/Programs/nssm/win32/nssm.exe';
 exports.ntrights = 'C:/Program Files (x86)/Windows Resource Kits/Tools/ntrights.exe';
 exports.vcredist_x86 = 'C:/Programs/vcredist_x86.exe';
+exports.libeay32 = fs.existsSync(path.join(process.env.SYSTEMROOT, 'System32', 'libeay32.dll'))
+  ? path.join(process.env.SYSTEMROOT, 'system32', 'libeay32.dll')
+  : 'C:/OpenSSL-Win32/libeay32.dll';
 exports.serviceUser = '';
 exports.servicePass = '';
 exports.serverPort = 1337;
