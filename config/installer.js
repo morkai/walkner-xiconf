b1855570aba16c022fcc014808d086046e5065bc
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2014-10-18 03:00:54 +0200

    Updated paths to various tools.

--- config/installer.js
+++ config/installer.js
@@ -5,15 +5,15 @@ var path = require('path');
 
 exports.autoIt3Wrapper = 'C:/Program Files (x86)/AutoIt3/SciTE/AutoIt3Wrapper/AutoIt3Wrapper.exe';
 exports.makensis = 'C:/Program Files (x86)/NSIS/makensis.exe';
-exports.googleChromePortable = 'C:/Programs/GoogleChromePortable';
+exports.googleChromePortable = 'C:/tools/GoogleChromePortable';
 exports.node = 'C:/Program Files (x86)/nodejs/node.exe';
-exports.nssm = 'C:/Programs/nssm/win32/nssm.exe';
+exports.nssm = 'C:/tools/nssm/win32/nssm.exe';
 exports.ntrights = 'C:/Program Files (x86)/Windows Resource Kits/Tools/ntrights.exe';
-exports.vcredist_x86 = 'C:/Programs/vcredist_x86.exe';
+exports.vcredist_x86 = 'C:/tools/vcredist_x86.exe';
 exports.libeay32 = fs.existsSync(path.join(process.env.SYSTEMROOT, 'System32', 'libeay32.dll'))
   ? path.join(process.env.SYSTEMROOT, 'system32', 'libeay32.dll')
-  : 'C:/OpenSSL-Win32/libeay32.dll';
-exports.imWorkin = 'C:/Programs/ImWorkin.exe';
+  : 'C:/tools/OpenSSL-Win32/libeay32.dll';
+exports.imWorkin = 'C:/tools/ImWorkin.exe';
 exports.serviceUser = '';
 exports.servicePass = '';
 exports.serverPort = 1337;
